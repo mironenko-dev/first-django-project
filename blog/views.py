fab77517f334c8f0bd2d3af69fa3aba5f10ff08d
mironenko-dev <mironenko.for.dev@gmail.com>
2022-05-12 16:58:11 +0300

    6th day of coding, added styles for forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
+
 
 """def home(request):
     return render(request, 'home.html', {})"""
@@ -18,5 +20,6 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    #fields = '__all__'
